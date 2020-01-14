8e0170fc4e59935bb9cb17e660e1d8a58e782fb1
heajing <heajing518@163.com>
2020-01-14 19:34:54 +0800

    改

--- hello-spring-cloud-web-admin-feign/src/main/java/cn/test/spring/cloud/web/admin/feign/controller/AdminController.java
+++ hello-spring-cloud-web-admin-feign/src/main/java/cn/test/spring/cloud/web/admin/feign/controller/AdminController.java
@@ -2,6 +2,7 @@ package cn.test.spring.cloud.web.admin.feign.controller;
 
 import cn.test.spring.cloud.web.admin.feign.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdminController {
 
+
+    @Qualifier("adminServiceHystrix")
     @Autowired
     private AdminService adminService;
 
