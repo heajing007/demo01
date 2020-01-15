f4a87ee24d75f85baf40109b807227aa2783cee0
heajing <heajing518@163.com>
2020-01-15 10:29:35 +0800

    改

--- hello-spring-cloud-web-admin-feign/src/main/java/cn/test/spring/cloud/web/admin/feign/controller/AdminController.java
+++ hello-spring-cloud-web-admin-feign/src/main/java/cn/test/spring/cloud/web/admin/feign/controller/AdminController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminController {
 
 
-    @Qualifier("adminServiceHystrix")
     @Autowired
     private AdminService adminService;
 
